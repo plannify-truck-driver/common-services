4fb14dd442d50e852e28968bf785ef5df9cec1f2
Baptiste Bronsin <baptiste.bronsin@outlook.com>
2026-06-21 21:42:45 +0200

    fix: update default language to 'en' in drivers table migration

--- database-migrations/20250823134203_create_drivers_table.sql
+++ database-migrations/20250823134203_create_drivers_table.sql
@@ -9,12 +9,11 @@ CREATE TABLE IF NOT EXISTS public."drivers" (
     phone_number VARCHAR(20),
     is_searchable BOOLEAN DEFAULT true NOT NULL,
     allow_request_professional_agreement BOOLEAN DEFAULT false NOT NULL,
-    language VARCHAR(10) DEFAULT 'fr' NOT NULL,
+    language VARCHAR(10) DEFAULT 'en' NOT NULL,
     rest_json JSONB,
     mail_preferences INTEGER DEFAULT 0 NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
     verified_at TIMESTAMP WITH TIME ZONE,
     last_login_at TIMESTAMP WITH TIME ZONE,
-    deactivated_at TIMESTAMP WITH TIME ZONE,
-    refresh_token_hash TEXT
+    deactivated_at TIMESTAMP WITH TIME ZONE
 );
